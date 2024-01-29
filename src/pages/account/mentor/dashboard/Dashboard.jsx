d378366ba40c9bc7bd87aa40de39a3a04930b4ba
ck2135 <charankatakam8@gmail.com>
2024-01-29 20:56:29 +0530

    made changes

--- src/pages/account/mentor/dashboard/Dashboard.jsx
+++ src/pages/account/mentor/dashboard/Dashboard.jsx
@@ -1,21 +1,9 @@
 import React from 'react'
-import "./Dashboard.css"
-import Card from './components/cards/Card'
-import Navbar from './components/Navbar/Navbar'
-
 
 const Dashboard = () => {
   return (
-    <div className='dashboard'>
-      <div className="sideBar"></div>
-
-      <div className="right">
-        <div className="navbar"><Navbar/></div>
-
-        <div className="content">
-          <Card />
-        </div>
-      </div>
+    <div>
+      
     </div>
   )
 }
